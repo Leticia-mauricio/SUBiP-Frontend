19576d578baa13db24ea396b1e267f83b11070f8
Leticia <leticia-mauricio@hotmail.com>
2026-06-26 10:47:03 +0000

    Jun 26, 2026, 7:47 AM

---
+++ src/app/features/emprestimo/pages/meus-emprestimos/meus-emprestimos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-emprestimos',
+  imports: [],
+  templateUrl: './meus-emprestimos.html',
+  styleUrl: './meus-emprestimos.css',
+})
+export class MeusEmprestimos {}
