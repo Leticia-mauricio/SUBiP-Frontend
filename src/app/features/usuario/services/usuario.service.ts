d6e429e89a0a1dca9e6e03f9539f10fbd4df7bc6
Leticia <leticia-mauricio@hotmail.com>
2026-06-28 17:53:33 +0000

    Jun 28, 2026, 2:53 PM

--- src/app/features/usuario/services/usuario.service.ts
+++ src/app/features/usuario/services/usuario.service.ts
@@ -1,4 +1,32 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
 
-@Service()
-export class UsuarioService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private readonly url = 'http://localhost:8080/usuarios';
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.url);
+  }
+
+  buscarPorId(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.url}/${id}`);
+  }
+
+  salvar(usuario: Usuario): Observable<Usuario> {
+    if (usuario.id) {
+      return this.http.put<Usuario>(`${this.url}/${usuario.id}`, usuario);
+    }
+    return this.http.post<Usuario>(this.url, usuario);
+  }
+
+  excluir(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
