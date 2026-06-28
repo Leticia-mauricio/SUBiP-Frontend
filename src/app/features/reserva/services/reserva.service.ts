99da841d9dbc7033fe1675752cd68397a0e53403
Leticia <leticia-mauricio@hotmail.com>
2026-06-28 19:49:09 +0000

    Jun 28, 2026, 4:49 PM

--- src/app/features/reserva/services/reserva.service.ts
+++ src/app/features/reserva/services/reserva.service.ts
@@ -1,54 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import { Reserva } from '../models/reserva';
+import { ReservaView } from '../models/reserva-view';
 import { MinhaReserva } from '../models/minha-reserva';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservaService {
+  private readonly apiUrl = 'http://localhost:8080/reservas';
 
-  private readonly apiUrl =
-    'http://localhost:8080/reservas';
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  listar(): Observable<Reserva[]> {
-    return this.http.get<Reserva[]>(this.apiUrl);
+  listar(): Observable<ReservaView[]> {
+    return this.http.get<ReservaView[]>(this.apiUrl);
   }
 
-  buscarPorId(id: number): Observable<Reserva> {
-    return this.http.get<Reserva>(
-      `${this.apiUrl}/${id}`
-    );
+  buscarPorId(id: number): Observable<ReservaView> {
+    return this.http.get<ReservaView>(`${this.apiUrl}/${id}`);
   }
 
-  salvar(reserva: Reserva): Observable<Reserva> {
-    return this.http.post<Reserva>(
-      this.apiUrl,
-      reserva
-    );
+  salvar(reserva: Reserva): Observable<ReservaView> {
+    return this.http.post<ReservaView>(this.apiUrl, reserva);
   }
 
-  cancelar(id: number): Observable<Reserva> {
-    return this.http.put<Reserva>(
-      `${this.apiUrl}/${id}/cancelamento`,
-      {}
-    );
+  cancelar(id: number): Observable<ReservaView> {
+    return this.http.put<ReservaView>(`${this.apiUrl}/${id}/cancelamento`, {});
   }
 
-  listarPorLeitor(
-    pessoaId: number
-  ): Observable<MinhaReserva[]> {
-
-    return this.http.get<MinhaReserva[]>(
-      `${this.apiUrl}/leitor/${pessoaId}`
-    );
-
+  listarPorLeitor(pessoaId: number): Observable<MinhaReserva[]> {
+    return this.http.get<MinhaReserva[]>(`${this.apiUrl}/leitor/${pessoaId}`);
   }
-
 }
